42d30fb608526654412594d23197d6b5e079702c
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2018-11-18 13:15:50 -0200

    minor changes in the style and dotenv

--- controllers/products_controller.js
+++ controllers/products_controller.js
@@ -1,11 +1,18 @@
 const Product = require('../models/products');
+const db = require('../database');
 
 exports.index = (request, response, next) => {
     Product.all(products => {
         response.render('products/index', {
             products,
             headTitle: 'Products'
-        })
+        }),
+        db.query('SELECT * FROM users WHERE id = $1', [1], (err, res) => {
+            if (err) {
+              return next(err)
+            }
+            res.send(res.rows[0])
+          })
     })
 }
 
