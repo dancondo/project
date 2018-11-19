311452f0483564f66741b494a35f1ce98a3b8f21
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2018-11-19 21:39:21 -0200

    user and carts model

--- models/carts.js
+++ models/carts.js
@@ -1,5 +1,13 @@
-module.exports = class Cart {
-    constructor() {
+const Sequelize = require('sequelize');
+const sequelize = require('../database/index');
 
+const Cart = sequelize.define('cart', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
     }
-}
+})
+
+module.exports = User;
--- models/users.js
+++ models/users.js
@@ -1,8 +1,21 @@
-module.exports = class User {
-    constructor(attributes = {}) {
-        this.username = attributes.username
+const Sequelize = require('sequelize');
+const sequelize = require('../database/index');
+
+const User = sequelize.define('user', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
     }
-    static all() {
-        
-    }
-}
+})
+
+module.exports = User;
